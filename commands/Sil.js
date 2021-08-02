31fcb1044456c645bb19dc21636867451561f03b
Upjddj <88151757+Upjddj@users.noreply.github.com>
2021-08-02 07:09:46 +0300

    Create Sil.js

---
+++ commands/Sil.js
@@ -0,0 +1,8 @@
+module.exports = {
+   name:"sil",
+   aliases:['clear']
+   code:`
+   $clear[$message]
+   $thumbnail[$userAvatar]
+   $description 
+   $message[1]] Kadar Mesaj <@$authorID> Tarafından Silindi
